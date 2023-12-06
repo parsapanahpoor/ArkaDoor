0005ca16b9905a3fad465990202ec3cb92eb2385
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-12-06 15:13:21 +0330

    before add Role

---
+++ ArkaDoor.Domain/Entities/Role/Role.cs
@@ -0,0 +1,13 @@
+﻿using ArkaDoor.Domain.Common;
+namespace Window.Domain.Entities.Account;
+
+public class Role : BaseEntities<ulong>
+{
+    #region Properties
+
+    public string  Title { get; set; }
+
+    public string  RoleUniqueName { get; set; }
+
+    #endregion
+}
