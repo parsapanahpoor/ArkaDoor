4bd7000d8a132445fa4ec250b170854dc53d61c2
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-12-10 13:53:14 +0330

    register

--- ArkaDoor.Domain/DTOs/SiteSide/Account/RegisterDTO.cs
+++ ArkaDoor.Domain/DTOs/SiteSide/Account/RegisterDTO.cs
@@ -9,20 +9,20 @@ public record UserRegisterDTO
 
     [MaxLength(200, ErrorMessage = "Please Enter {0} Less Than {1} Character")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-    [DisplayName("Password")]
+    [DisplayName("کلمه ی عبور")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 
     [MaxLength(200, ErrorMessage = "Please Enter {0} Less Than {1} Character")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-    [DisplayName("Re Password")]
+    [DisplayName("تکرار کلمه ی عبور")]
     [Compare("Password", ErrorMessage = "Password And Re Password Does Not Match")]
     [DataType(DataType.Password)]
     public string RePassword { get; set; }
 
     [MaxLength(200, ErrorMessage = "Please Enter {0} Less Than {1} Character")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-    [DisplayName("Mobile")]
+    [DisplayName("موبایل")]
     public string Mobile { get; set; }
 
     #endregion
