f6876ac57359e12e3ccd44093b4beeb477a49052
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-12-15 14:53:16 +0330

    Add Video entity

---
+++ ArkaDoor.Domain/Entities/Video/Video.cs
@@ -0,0 +1,22 @@
+﻿using ArkaDoor.Domain.Common;
+
+namespace ArkaDoor.Domain.Entities.Video;
+
+public sealed class Video : BaseEntities<ulong>
+{
+    #region properties
+
+    public string Title { get; set; }
+
+    public string ShortDescription { get; set; }
+
+    public string longDescription { get; set; }
+
+    public string File { get; set; }
+
+    public string? Picture { get; set; }
+
+    public bool ShowInLanding { get; set; }
+
+    #endregion
+}
---
+++ ArkaDoor.Domain/Entities/Video/VideoTag.cs
@@ -0,0 +1,16 @@
+﻿using ArkaDoor.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+namespace ArkaDoor.Domain.Entities.Video;
+
+public sealed class VideoTag : BaseEntities<ulong>
+{
+    #region properties
+
+    public ulong VideoId { get; set; }
+
+    [Display(Name = "تگ")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public string TagTitle { get; set; }
+
+    #endregion
+}
